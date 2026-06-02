41f2475a455e2bff4e159eadf44eefb490c852fd
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-02 20:39:29 +0300

    Add test_orderedmeta

---
+++ tests/test_orderedmeta.py
@@ -0,0 +1,8 @@
+from orderedeta import Exercise
+
+
+def test_orderedmeta():
+    exer = Exercise(name="bench", weight=77.5, reps=2)
+    assert exer.name == "bench"
+    assert exer.weight == 77.5
+    assert exer.reps == 2
