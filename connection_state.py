076512ad7c9ba24488837e18dd7092ac3366d342
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-06-03 10:48:48 +0300

    Implement overloaded funcions
    open, close, read, write

--- connection_state.py
+++ connection_state.py
@@ -9,7 +9,8 @@ class Connection:
 
 
 class ConnectionState:
-    pass
+    def __repr__(self):
+        return self.__class__.__name__
 
 
 class OpenState(ConnectionState):
@@ -20,21 +21,60 @@ class ClosedState(ConnectionState):
     pass
 
 
-@dispatch(Connection, ConnectionState)
-def open(conn, state):
-    pass
-
-
-@dispatch(Connection, ConnectionState)
-def close(conn, state):
-    pass
+@dispatch(Connection, ClosedState)
+def write(conn, state):
+    raise RuntimeError(f"{state}: cannot write")
 
 
 @dispatch(Connection, OpenState)
 def read(conn, state):
-    print("reading")
+    print(f"{state}: reading")
+
+
+@dispatch(Connection, ConnectionState)
+def write(conn, state):  # noqa: F811
+    print(f"{state}:  writing")
+
+
+@dispatch(Connection, ClosedState)
+def close(conn, state):  # noqa: F811
+    raise RuntimeError(f"{state}: cannot close")
 
 
 @dispatch(Connection, OpenState)
-def write(conn, state):
-    print("writing")
+def close(conn, state):  # noqa: F811
+    print(f"{state}: closing")
+
+
+@dispatch(Connection, ClosedState)
+def open(conn, state):
+    print(f"{state}: opening")
+
+
+@dispatch(Connection, OpenState)
+def open(conn, state):  # noqa: F811
+    raise RuntimeError(f"{state}: cannot open")
+
+
+if __name__ == "__main__":
+    conn = Connection()
+    state = ClosedState()
+    try:
+        write(conn, state)
+    except RuntimeError as exc:
+        print(f"*** {exc}")
+    open(conn, state)
+    state = OpenState()
+    try:
+        open(conn, state)
+    except RuntimeError as exc:
+        print(f"*** {exc}")
+
+    write(conn, state)
+    read(conn, state)
+    close(conn, state)
+    state = ClosedState()
+    try:
+        close(conn, state)
+    except RuntimeError as exc:
+        print(f"*** {exc}")
